47f85d482ee03b6d8848d03abba60ddbde1dd18d
mg73631 <Titanic1912>
2017-11-04 13:17:14 +0100

    MG

--- src/pl/waw/sgh/Main.java
+++ src/pl/waw/sgh/Main.java
@@ -1,9 +1,48 @@
 package pl.waw.sgh;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Witam!!!");
+
+        int a=5;
+        Integer aa=10;
+        Double d;
+        d=5d;
+
+        /*komentarz
+        * w dwóch liniach*/
+        //komentarz w linii
+
+        System.out.println("d=" +d);
+        String s1 = "abc";
+        String s2 = "def";
+        String s3 = s1.concat(s2);
+        System.out.println(s3);
+
+        boolean b = false;
+        boolean c = true;
+
+        //AND &&
+        // OR ||
+        //XOR albo albo
+
+        //BLOKI
+
+
+        {
+
+            int i =6;
+            System.out.println(i);
+            System.out.println("joł");
+
+
+        }
+
+
+
     }
 }
